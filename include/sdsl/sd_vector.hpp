51396386797de826d6e4bc4e362a4bc8428416a1
Andrea Farruggia <andrea86@gmail.com>
2015-11-11 00:28:15 +0100

    Revert "Fix crash when accessing empty sd_vector"
    This reverts commit 0b25bd354d4d44795800f834e39fd331ce2504ff.

--- include/sdsl/sd_vector.hpp
+++ include/sdsl/sd_vector.hpp
@@ -281,8 +281,6 @@ class sd_vector
             builder = sd_vector_builder();
         }
 
-        bool is_empty() const { return m_low.empty(); }
-
         //! Accessing the i-th element of the original bit_vector
         /*! \param i An index i with \f$ 0 \leq i < size()  \f$.
         *   \return The i-th bit of the original bit_vector
@@ -295,9 +293,6 @@ class sd_vector
         */
         value_type operator[](size_type i)const
         {
-            if (is_empty()) {
-                return value_type{};
-            }
             size_type high_val = (i >> (m_wl));
             size_type sel_high = m_high_0_select(high_val + 1);
             size_type rank_low = sel_high - high_val;
@@ -324,9 +319,6 @@ class sd_vector
          */
         uint64_t get_int(size_type idx, const uint8_t len=64) const
         {
-            if (is_empty()) {
-                return 0UL;
-            }
             uint64_t i = idx+len-1;
             uint64_t high_val = (i >> (m_wl));
             uint64_t sel_high = m_high_0_select(high_val + 1);
@@ -495,11 +487,6 @@ class rank_support_sd
         size_type rank(size_type i)const
         {
             assert(m_v != nullptr);
-
-            if (m_v->is_empty()) {
-                return 0UL;
-            }
-
             assert(i <= m_v->size());
             // split problem in two parts:
             // (1) find  >=
